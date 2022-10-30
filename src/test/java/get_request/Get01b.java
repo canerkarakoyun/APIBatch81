7900154eab4d0de2ebcf58e02bf8991bc5484de2
Caner Karakoyun <caner.karakoyun@gmail.com>
2022-10-31 00:00:00 +0300

    V08

---
+++ src/test/java/get_request/Get01b.java
@@ -0,0 +1,42 @@
+package get_request;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class Get01b {
+    /*
+        Given
+            https://reqres.in/api/users/3
+        When
+            User sends a GET Request to the url
+        Then
+            HTTP Status Code should be 200
+        And
+            Content Type should be JSON
+        And
+            Status Line should be HTTP/1.1 200 OK
+     */
+    @Test
+    public  void get01(){
+        // Set the url
+        String url = "https://reqres.in/api/users/3";
+
+        // Set the expected data
+
+        // Send the response and get the response
+        Response response=given().when().get(url);
+        response.prettyPrint();
+
+        // Do assertion
+        response.
+                then().
+                assertThat().
+                statusCode(200).
+                contentType(ContentType.JSON).
+                statusLine("HTTP/1.1 200 OK");
+
+    }
+}
---
+++ src/test/java/get_request/Get02b.java
@@ -0,0 +1,46 @@
+package get_request;
+
+import base_url.ReqresBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+public class Get02b extends ReqresBaseUrl {
+    /*
+        Given
+            https://reqres.in/api/users/23
+        When
+            User send a GET Request to the url
+        Then
+            HTTP Status code should be 404
+        And
+            Status Line should be HTTP/1.1 404 Not Found
+        And
+            Server is "cloudflare"
+        And
+            Response body should be empty
+     */
+    @Test
+    public void get01(){
+    // set the url
+        spec.pathParams("first","api", "second", "users", "third", 23);
+
+    // set the expected data
+
+    // send the request and get the response
+        Response response = given().spec(spec).get("/{first}/{second}/{third}");
+        response.prettyPrint();
+
+    // do assertion
+
+        assertEquals(404, response.statusCode());
+        assertEquals("HTTP/1.1 404 Not Found", response.statusLine());
+        assertEquals("cloudflare", response.getHeader("Server"));
+        assertEquals(2, response.asString().replaceAll("\\s","").length());
+
+
+    }
+
+}
