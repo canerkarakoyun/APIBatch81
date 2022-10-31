9be7f62c2114cb9a33c2ec3b720a3b82f455968b
Caner Karakoyun <caner.karakoyun@gmail.com>
2022-10-31 23:55:08 +0300

    V11

--- src/test/java/get_request/Get06b.java
+++ src/test/java/get_request/Get06b.java
@@ -6,6 +6,8 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.junit.Test;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.*;
 import static org.junit.Assert.*;
 
@@ -35,6 +37,25 @@ public class Get06b extends ReqresBaseUrl {
         assertEquals(200, response.getStatusCode());
 
         JsonPath jsonPath = response.jsonPath();
+
+        // 1)Status code is 200
+        assertEquals(200, response.getStatusCode());
+
+        // 2)Print all pantone_values
         System.out.println(jsonPath.getList("data.pantone_value"));
+
+        // 3)Print all ids greater than 3 on the console
+        List<Integer> ids = jsonPath.getList("data.findAll{it.id>3}.id");
+        System.out.println("ids = " + ids);
+
+        // Assert that there are 3 ids greater than 3
+        assertEquals(3, ids.size());
+
+        // 4)Print all names whose ids are less than 3 on the console
+        List<String> names = jsonPath.getList("data.findAll{it.id<3}.name");
+        System.out.println("names = " + names);
+
+        // Assert that the number of names whose ids are less than 3 is 2
+        assertEquals(2, names.size());
     }
 }
