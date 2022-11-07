ccbbf8962bcbde21c251e2bde03caea583942f5f
Caner Karakoyun <caner.karakoyun@gmail.com>
2022-11-07 23:42:03 +0300

    V17

--- src/test/java/post_requests/Post01.java
+++ src/test/java/post_requests/Post01.java
@@ -1,18 +1,30 @@
 package post_requests;
 
-public class Post01 {
+import base_url.JsonplaceholderBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Test;
+import test_data.JsonPlaceHolderTestData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+public class Post01 extends JsonplaceholderBaseUrl {
     /*
     Given
       1) https://jsonplaceholder.typicode.com/todos
-      2)
-   When
-       I send POST Request to the Url
-   Then
-       Status code is 201 {
+      2) {
             "userId": 55,
             "title": "Tidy your room",
             "completed": false
                          }
+   When
+       I send POST Request to the Url
+   Then
+       Status code is 201
    And
        response body is like {
                                "userId": 55,
@@ -22,6 +34,25 @@ public class Post01 {
                                }
 */
 
+    @Test
+    public void post01() {
+        // Set the url
+        spec.pathParam("first", "todos");
 
+        // set the expected data
+        JsonPlaceHolderTestData obj = new JsonPlaceHolderTestData();
+        Map<String, Object> expectedData = obj.expectedDataMethod(55, "Tidy your room", false);
 
+        // send the request and get the response
+        Response response = given().spec(spec).contentType(ContentType.JSON).body(expectedData).when().post("/{first");
+        response.prettyPrint();
+
+        // Do assertions
+        Map<String,Object> actualData = response.as(HashMap.class);
+
+        assertEquals(expectedData.get("completed"), actualData.get("completed"));
+        assertEquals(expectedData.get("title"), actualData.get("title"));
+        assertEquals(expectedData.get("userId"), actualData.get("userId"));
+
+    }
 }
