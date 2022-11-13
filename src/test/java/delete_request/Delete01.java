196778a265f47e8771805d7918e2ba4f06065aea
Caner Karakoyun <caner.karakoyun@gmail.com>
2022-11-14 00:07:33 +0300

    V30

--- src/test/java/delete_request/Delete01.java
+++ src/test/java/delete_request/Delete01.java
@@ -3,8 +3,13 @@ package delete_request;
 import base_url.JsonplaceholderBaseUrl;
 import io.restassured.response.Response;
 import org.junit.Test;
+import utils.ObjectMapperUtils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.Assert.*;
 
 public class Delete01 extends JsonplaceholderBaseUrl {
     /*
@@ -23,10 +28,23 @@ public class Delete01 extends JsonplaceholderBaseUrl {
         spec.pathParams("first","todos","second",198);
 
         // set the expected data
+        Map<String,String> expectedData = new HashMap<>();
 
         // Send request and get response
         Response response=given().spec(spec).when().delete("/{first}/{second}");
 
+        // Do assertions
+        Map actualData= ObjectMapperUtils.convertJsonToJava(response.asString(),Map.class);
 
+        assertEquals(200, response.getStatusCode());
+
+        // 1. Yol
+        assertEquals(expectedData, actualData);
+
+        // 2. yol
+        assertTrue(actualData.isEmpty());
+
+        // 3. yol
+        assertEquals(0, actualData.size());
     }
 }
